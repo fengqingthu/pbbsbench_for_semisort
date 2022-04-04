81fc5b7dc1c1ffdbd3ada8e4bc2f3d0df9d8eba6
fengqing <fengqingthu@gmail.com>
2022-04-04 01:03:07 -0400

    Fix distribution generator

--- testData/sequenceData/expDistSeq.cpp
+++ testData/sequenceData/expDistSeq.cpp
@@ -7,10 +7,10 @@ using parlay::parallel_for;
 
 int main(int argc, char* argv[]) {
     
-    commandLine P(argc,argv,"[-r <range>] [-t {uint64_t}] <size> <outfile>");
+    commandLine P(argc,argv,"[-r <range>] [-t {unsigned long}] <size> <outfile>");
     pair<size_t,char*> in = P.sizeAndFileName();
     size_t para = std::atoi(P.getArgument(2));
-    // element type is fixed to uint64_t, which is not included in the elementTypeFromString function return value
+    // element type is fixed to unsigned long, which is not included in the elementTypeFromString function return value
     size_t n = in.first;
     char* fname = in.second;
 
@@ -18,8 +18,8 @@ int main(int argc, char* argv[]) {
     std::default_random_engine generator;
     std::exponential_distribution<double> distribution(para);
 
-    parallel_for(0, arr.size(), [&](size_t i) {
-        arr[i] = static_cast<unsigned long>(arr.size() * distribution(generator));
+    parallel_for(0, n, [&](size_t i) {
+        arr[i] = static_cast<unsigned long>(n * distribution(generator));
     });
 
 
